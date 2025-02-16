a49faac2f4a64c8236a473ea3d39ee6d91f1c9b9
KAGISO KEGOPE MOTHIBEDI <mothibedik@btc.bw>
2025-02-16 19:47:49 +0200

    Added code from previous project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" /> */}
 
-        <Weather city="Paris" />
-      </header>
+      <Weather city="Paris" />
+      {/* </header> */}
+      <p>
+        Open-source code, by{" "}
+        <a href="https://github.com/KagisoMothibedi">Kagiso Mothibedi</a> from{" "}
+        <a href="shecodes.io"> Shecodes</a>
+      </p>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,22 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { Audio, Rings } from "react-loader-spinner";
+import "./Weather.css";
 
-export default function Weather(props) {
+export default function Weather() {
+  let [sCity, setCity] = useState(null);
+  let [display, setDisplay] = useState(null);
+  let [temperature, setTemperature] = useState(null);
+  let [description, setDescription] = useState(null);
+  let [humidity, setHumidity] = useState(null);
+  let [wind, setWind] = useState(null);
+  let [icon, setIcon] = useState(null);
+
   function showTemperature(response) {
-    alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
+    setTemperature(response.data.main.temp);
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+    setIcon(
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
   }
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=7746bdeabca928cfedcad71e52fd9d66&units=metric`;
-  axios.get(url).then(showTemperature);
+
+  function handleFunction(event) {
+    event.preventDefault();
+    // setDisplay(`It is °C in ${sCity}`);
+    if (temperature) {
+      return setDisplay(
+        <ul>
+          <li>Temperature: {temperature}°C</li>
+          <li>Description: {description}</li>
+          <li>Humidity: {humidity}% </li>
+          <li>Wind: {wind}mph</li>
+          <li>
+            <img src={icon} alt={description} />
+            {/* {`http://openweathermap.org/img/wn/${icon}@2x.png`} */}
+            {/* <ReactAnimatedWeather
+              icon="RAIN"
+              color="goldenrod"
+              size={50}
+              animate={true}
+            /> */}
+          </li>
+        </ul>
+
+        // <p>
+        //   {temperature}°C in {sCity}
+        // </p>
+
+        // return (
+        //   <h1>
+        //     The temperature in {sCity} is {Math.round(temperature)}°C{" "}
+        //   </h1>
+      );
+    } else {
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${sCity}&appid=7746bdeabca928cfedcad71e52fd9d66&units=metric`;
+      axios.get(url).then(showTemperature);
+      <p>Loading</p>;
+    }
+    // return setDisplay(
+    //   <p>
+    //     Hello {temperature} at {sCity}
+    //   </p>
+    // )
+  }
+
+  function captureCity(event) {
+    setCity(event.target.value);
+  }
+
   return (
-    <Rings
-      visible={true}
-      height="80"
-      width="80"
-      color="#4fa94d"
-      ariaLabel="rings-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-    />
+    <div className="Weather">
+      <form onSubmit={handleFunction}>
+        <input
+          type="search"
+          placeholder="Enter a city..."
+          onChange={captureCity}
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <div className="displayTemperatures">{display}</div>
+    </div>
   );
 }
